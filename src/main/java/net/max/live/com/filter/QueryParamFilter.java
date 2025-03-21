e45fed72305a043abc346576f65e3ba507196abd
neoashraf <ashrafuddin160@gmail.com>
2025-03-22 02:16:23 +0600

    update: public api end point device status

--- src/main/java/net/max/live/com/filter/QueryParamFilter.java
+++ src/main/java/net/max/live/com/filter/QueryParamFilter.java
@@ -106,7 +106,7 @@ public class QueryParamFilter extends AbstractGatewayFilterFactory<QueryParamFil
             String path = request.getURI().getPath();
 
             // Skip filtering only for the specific public API
-            if (path.matches("^/business/api/v1/devices/[a-f0-9\\-]+/status$")) {
+            if (path.matches("^/business/api/v1/devices/status$")) {
                 return chain.filter(exchange);
             }
 
